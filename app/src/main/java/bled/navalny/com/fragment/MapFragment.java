9d741a0d82ad24ba2534aafcec51fd9360a2b0b7
Persick <opersick@yandex.ru>
2017-08-12 17:14:27 +0300

    изменения от персика

---
+++ app/src/main/java/bled/navalny/com/fragment/MapFragment.java
@@ -0,0 +1,32 @@
+package com.example.persick.navalnyandroid.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.persick.navalnyandroid.R;
+
+/**
+ * Created by persick on 12/08/2017.
+ */
+
+public class MapFragment extends Fragment {
+        public MapFragment() {
+            // Required empty public constructor
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_map, container, false);
+        }
+}
---
+++ app/src/main/java/bled/navalny/com/fragment/NotificationsFragment.java
@@ -0,0 +1,31 @@
+package com.example.persick.navalnyandroid.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.persick.navalnyandroid.R;
+
+/**
+ * Created by persick on 12/08/2017.
+ */
+
+public class NotificationsFragment extends Fragment {
+    public NotificationsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
+    }
+}
