87620f5a682e45bedced0ca4228d95add436f8cc
Persick <opersick@yandex.ru>
2017-08-12 20:23:20 +0300

    fragment profile is done

--- app/src/main/java/bled/navalny/com/fragment/ProfileFragment.java
+++ app/src/main/java/bled/navalny/com/fragment/ProfileFragment.java
@@ -5,60 +5,71 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import bled.navalny.com.R;
 import butterknife.BindView;
-import ru.tinkoff.decoro.MaskImpl;
-import ru.tinkoff.decoro.slots.PredefinedSlots;
-import ru.tinkoff.decoro.watchers.FormatWatcher;
-import ru.tinkoff.decoro.watchers.MaskFormatWatcher;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * Created by persick on 12/08/2017.
  */
 
-public class ProfileFragment extends Fragment
-{
-	@BindView(R.id.headerLayout)
-	LinearLayout headerLayout;
-	@BindView(R.id.phoneNumberEditText)
-	EditText phoneNumberEditText;
+public class ProfileFragment extends Fragment {
+    @BindView(R.id.headerLayout)
+    LinearLayout headerLayout;
+    @BindView(R.id.phoneNumberTextView)
+    TextView phoneNumberTextView;
 
-	butterknife.Unbinder unbinder;
+    Unbinder unbinder;
+    @BindView(R.id.permissionSwitch)
+    Switch permissionSwitch;
+    @BindView(R.id.chooseTimeRadioGroup)
+    RadioGroup chooseTimeRadioGroup;
 
-	public ProfileFragment()
-	{
-		// Required empty public constructor
-	}
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState)
-	{
-		// Inflate the layout for this fragment
-		View view = inflater.inflate(R.layout.fragment_profile, container, false);
-		unbinder = butterknife.ButterKnife.bind(this, view);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        unbinder = ButterKnife.bind(this, view);
 
-        FormatWatcher formatWatcher = new MaskFormatWatcher(
-                MaskImpl.createTerminated(PredefinedSlots.RUS_PHONE_NUMBER) // маска для серии и номера
-        );
-        formatWatcher.installOn(phoneNumberEditText);
+//        FormatWatcher formatWatcher = new MaskFormatWatcher(
+//                MaskImpl.createTerminated(PredefinedSlots.RUS_PHONE_NUMBER) // маска для серии и номера
+//        );
+//        formatWatcher.installOn(phoneNumberTextView);
 
-		return view;
-	}
+        onViewClicked();
 
-	@Override
-	public void onDestroyView()
-	{
-		super.onDestroyView();
-		unbinder.unbind();
-	}
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.permissionSwitch)
+    public void onViewClicked() {
+        if (permissionSwitch.isChecked()) {
+            chooseTimeRadioGroup.setVisibility(View.VISIBLE);
+        } else {
+            chooseTimeRadioGroup.setVisibility(View.GONE);
+        }
+    }
 }
