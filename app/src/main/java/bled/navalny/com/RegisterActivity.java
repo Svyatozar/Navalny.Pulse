0d4e93d1cac595c7e88ed3866eb373240f2e9bea
Svyatozar <konservator007@gmail.com>
2017-08-12 20:33:01 +0300

    экран регистрации

---
+++ app/src/main/java/bled/navalny/com/RegisterActivity.java
@@ -0,0 +1,13 @@
+package bled.navalny.com;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+		super.onCreate(savedInstanceState, persistentState);
+	}
+}
