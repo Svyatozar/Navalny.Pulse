a9a317556545912266d66d7692ab485b2252b88e
Svyatozar <konservator007@gmail.com>
2017-08-13 15:56:27 +0300

    Добавил токен в заголовки

--- app/src/main/java/bled/navalny/com/ApplicationWrapper.java
+++ app/src/main/java/bled/navalny/com/ApplicationWrapper.java
@@ -3,7 +3,14 @@ package bled.navalny.com;
 import android.app.Application;
 import android.content.Context;
 
+import java.io.IOException;
+
 import bled.navalny.com.api.BledService;
+import bled.navalny.com.helpers.SharedPreferenceHelper;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -17,15 +24,30 @@ public class ApplicationWrapper extends Application {
     public static BledService bledService;
     public static Context context;
 
-    public static String token;
-
     @Override
     public void onCreate() {
         super.onCreate();
         context =  getApplicationContext();
 
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(new Interceptor() {
+                                      @Override
+                                      public Response intercept(Interceptor.Chain chain) throws IOException
+                                      {
+                                          Request original = chain.request();
+
+                                          Request request = original.newBuilder()
+                                                  .header("Pulse-Auth-Token", SharedPreferenceHelper.getToken())
+                                                  .method(original.method(), original.body())
+                                                  .build();
+
+                                          return chain.proceed(request);
+                                      }
+                                  });
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(httpClient.build())
                 .addConverterFactory(GsonConverterFactory.create())
                 //.client(httpClient.build()) // network requests logging
                 .build();
---
+++ app/src/main/java/bled/navalny/com/helpers/SharedPreferenceHelper.java
@@ -0,0 +1,92 @@
+package bled.navalny.com.helpers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import bled.navalny.com.ApplicationWrapper;
+
+/**
+ * Created by svyatozar on 13.08.17.
+ */
+
+public class SharedPreferenceHelper {
+	private final static String PREF_FILE = "PREF";
+	private final static String TOKEN_KEY = "TOKEN_KEY";
+
+	public static void setToken(String token) {
+		setSharedPreferenceString(ApplicationWrapper.context, TOKEN_KEY, token);
+	}
+
+	public static String getToken() {
+		return getSharedPreferenceString(ApplicationWrapper.context, TOKEN_KEY, "");
+	}
+
+	/**
+	 * Set a string shared preference
+	 * @param key - Key to set shared preference
+	 * @param value - Value for the key
+	 */
+	static void setSharedPreferenceString(Context context, String key, String value){
+		SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putString(key, value);
+		editor.apply();
+	}
+
+	/**
+	 * Set a integer shared preference
+	 * @param key - Key to set shared preference
+	 * @param value - Value for the key
+	 */
+	static void setSharedPreferenceInt(Context context, String key, int value){
+		SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putInt(key, value);
+		editor.apply();
+	}
+
+	/**
+	 * Set a Boolean shared preference
+	 * @param key - Key to set shared preference
+	 * @param value - Value for the key
+	 */
+	static void setSharedPreferenceBoolean(Context context, String key, boolean value){
+		SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putBoolean(key, value);
+		editor.apply();
+	}
+
+	/**
+	 * Get a string shared preference
+	 * @param key - Key to look up in shared preferences.
+	 * @param defValue - Default value to be returned if shared preference isn't found.
+	 * @return value - String containing value of the shared preference if found.
+	 */
+	static String getSharedPreferenceString(Context context, String key, String defValue){
+		SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+		return settings.getString(key, defValue);
+	}
+
+	/**
+	 * Get a integer shared preference
+	 * @param key - Key to look up in shared preferences.
+	 * @param defValue - Default value to be returned if shared preference isn't found.
+	 * @return value - String containing value of the shared preference if found.
+	 */
+	static int getSharedPreferenceInt(Context context, String key, int defValue){
+		SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+		return settings.getInt(key, defValue);
+	}
+
+	/**
+	 * Get a boolean shared preference
+	 * @param key - Key to look up in shared preferences.
+	 * @param defValue - Default value to be returned if shared preference isn't found.
+	 * @return value - String containing value of the shared preference if found.
+	 */
+	static boolean getSharedPreferenceBoolean(Context context, String key, boolean defValue){
+		SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+		return settings.getBoolean(key, defValue);
+	}
+}
